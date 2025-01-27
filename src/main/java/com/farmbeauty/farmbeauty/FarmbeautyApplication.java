62f420f176a9a86a22be79bba3e188878b41cf50
Beatriz <beatriizsantina14@gmail.com>
2025-01-27 10:12:08 -0300

    configuração inicial do projeto

---
+++ src/main/java/com/farmbeauty/farmbeauty/FarmbeautyApplication.java
@@ -0,0 +1,13 @@
+package com.farmbeauty.farmbeauty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmbeautyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmbeautyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/farmbeauty/farmbeauty/FarmbeautyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmbeauty.farmbeauty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmbeautyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
